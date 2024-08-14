6b8fd77c42ec20b38bb37ff096d66fa71ae07155
RodrigoMartiQ <rodrigomarti2015@gmail.com>
2024-08-14 03:05:19 -0300

    comienzo de proyecto

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./sections/navbar/Navbar";
+import "./styles/style.css"
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Navbar/>
+);
---
+++ src/sections/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import  style from "./style.module.css"
+
+function Navbar(){
+
+return(
+
+<div>
+    <h1 className={style.letra}>
+        Hola padreee
+    </h1>
+</div>
+
+
+);
+}
+export default Navbar;
