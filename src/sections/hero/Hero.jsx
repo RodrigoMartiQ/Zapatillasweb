a275a5e9edbd5dc8a40f876e41afe8ec731ecd42
RodrigoMartiQ <rodrigomarti2015@gmail.com>
2024-08-16 17:02:58 -0300

    haciendo carrusel hero

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -2,11 +2,14 @@ import  style from "./style.module.css"
 
 function Hero(){
 return(
+    <div className={style.container}>
     <div>
         <img className={style.imagen1 }src="./public/imagenes/zaparoja.jpg" alt="" />
+        <div>
         <h1 className={style.holapedro}>HOLA  PEDRO</h1>
+        </div>
     </div>
-
+    </div>
 
 
 
