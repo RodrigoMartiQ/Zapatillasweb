c7964253735434dd93632f93fb4efee2bfa69158
RodrigoMartiQ <rodrigomarti2015@gmail.com>
2024-08-22 23:36:59 -0300

    mejoras en el card de zapas

--- src/components/zapas-card/zapas-card.jsx
+++ src/components/zapas-card/zapas-card.jsx
@@ -5,9 +5,10 @@ export default function BurgerCard({ title, img, price }) {
     <div className={style.container}>
       <a className={style.favorites}><i class="bi bi-heart"></i></a>
       <img src={img} alt="zapas" className={style.image} />
+      <div className={style.texto}>
       <h3 className={style.title}>{title}</h3>
       <p className={style.price}>$ {price}</p>
-      
+      </div>
     </div>
   );
 }
