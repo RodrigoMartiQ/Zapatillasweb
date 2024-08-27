90c965816f513fbbe36966c7f7a9a826f41e0b59
RodrigoMartiQ <rodrigomarti2015@gmail.com>
2024-08-27 18:21:18 -0300

    responsive

--- src/sections/photomodel/Photomodel.jsx
+++ src/sections/photomodel/Photomodel.jsx
@@ -6,7 +6,7 @@ function Photomodel() {
       <div className={style.todo}>
         <h1 className={style.texto}>Comprar por coleccion</h1>
         <div className={style.fotosmodelos}>
-        <img className={style.fotos} src="./public/imagenes/poses/pose1.jpg" />
+        <img className={style.fotos1} src="./public/imagenes/poses/pose1.jpg" />
         <img className={style.fotos} src="./public/imagenes/poses/pose2.jpg" />
         <img className={style.fotos} src="./public/imagenes/poses/pose3.jpg" />
         </div>
